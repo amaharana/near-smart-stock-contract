6437c64171e70d2557b4dc720aff29e6fd5b6dd1
amaharana <amaharana@users.noreply.github.com>
2022-03-25 06:16:57 -0700

    Accept contract name to deploy as parameter in deploy-stock-contract.js deployment script

--- contract/deploy-stock-contract.js
+++ contract/deploy-stock-contract.js
@@ -2,8 +2,8 @@ const { exit } = require('process');
 const fs = require('fs');
 
 /**
- * Run from project root directory:
- *      near repl -s ./contract/deploy-stock-contract.js -- company-a.mtestaccount.testnet multisig.commpany-a.mtestaccount.testnet
+ * Run from project root directory. Example command and arguments:
+ *      near repl -s ./contract/deploy-stock-contract.js -- company-a.mtestaccount.testnet stockcontract1 multisig.commpany-a.mtestaccount.testnet
  */
 
 var arguments = process.argv ;
@@ -11,17 +11,17 @@ var arguments = process.argv ;
 // arguments.forEach(arg => console.log(arg));
 
 // first arg is path to "node", second arg is path to "near" and so on...
-if (arguments.length != 8) {
-    console.log("USAGE: near repl -s ./contract/deploy-stock-contract.js -- <deployment_account_id> <multisig_account_id>");
+if (arguments.length != 9) {
+    console.log("USAGE: near repl -s ./contract/deploy-stock-contract.js -- <deployment_account_id> <stock_contract_name> <multisig_account_id>");
 }
 
 const deployerAccountName = arguments[6]; // "company-a.mtestaccount.testnet";
-const contractName = "stockcontract." + deployerAccountName; //stockcontract.company-a.mtestaccount.testnet";
+const contractName = arguments[7] + "." + deployerAccountName; //stockcontract.company-a.mtestaccount.testnet";
 const initArgs = {
     "ticker": "MYCOMPANY",
     "total_shares": 100000,
     "price_per_share": 2,
-    "allowed_admin_caller": arguments[7]
+    "allowed_admin_caller": arguments[8]
 };
 
 exports.main = async function(context) {
